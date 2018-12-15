1e442199748588e46d5195bdedbcf943757f67a8
Sasa Juric <sasa.juric@gmail.com>
2018-12-15 23:42:39 +0100

    optimize 201814

--- lib/2018/day14.ex
+++ lib/2018/day14.ex
@@ -1,15 +1,11 @@
 defmodule Aoc201814 do
   def run() do
-    part1()
-    part2()
+    IO.puts(part1())
+    IO.puts(part2())
   end
 
-  defp part1(), do: recipes() |> Stream.drop(306_281) |> Enum.take(10) |> Enum.join("") |> IO.puts()
-
-  defp part2() do
-    target_recipes = Enum.map('306281', &(&1 - ?0))
-    find_sublist_index(recipes(), target_recipes) |> IO.puts()
-  end
+  defp part1(), do: recipes() |> Stream.drop(306_281) |> Enum.take(10) |> Enum.join("")
+  defp part2(), do: recipes() |> find_sublist_index(Integer.digits(306_281))
 
   def find_sublist_index(enumerable, sublist) do
     Enum.reduce_while(
@@ -24,48 +20,34 @@ defmodule Aoc201814 do
     )
   end
 
-  defp recipes(), do: Stream.resource(&initial_state/0, &next_recipes/1, &destroy_state/1)
+  require Record
+  Record.defrecord(:state, [:elf1_pos, :elf2_pos, :recipes, :new_recipes])
 
-  defp initial_state() do
-    initial_recipes = [3, 7]
-    recipes = :ets.new(:recipes, [:private, read_concurrency: true, write_concurrency: true])
-    add_recipes(recipes, initial_recipes)
-    {0, 1, recipes, initial_recipes}
-  end
+  defp recipes(), do: Stream.resource(&initial_state/0, &{state(&1, :new_recipes), next_recipes(&1)}, fn _ -> :ok end)
 
-  defp destroy_state({_elf1_pos, _elf2_pos, recipes, _new_recipes}), do: :ets.delete(recipes)
+  defp initial_state(), do: state(elf1_pos: 0, elf2_pos: 1, recipes: <<3, 7>>, new_recipes: [3, 7])
 
-  defp next_recipes({elf1_pos, elf2_pos, recipes, previous_new_recipes}) do
+  defp next_recipes(state(elf1_pos: elf1_pos, elf2_pos: elf2_pos, recipes: recipes)) do
     elf1_recipe = recipe_at(recipes, elf1_pos)
     elf2_recipe = recipe_at(recipes, elf2_pos)
 
-    new_recipes =
+    {new_recipes, recipes} =
       case elf1_recipe + elf2_recipe do
         new_recipe when new_recipe < 10 ->
-          :ets.insert(recipes, {num_recipes(recipes), new_recipe})
-          [new_recipe]
+          {[new_recipe], <<recipes::binary, new_recipe>>}
 
         new_recipe ->
-          new_recipes = [1, new_recipe - 10]
-          add_recipes(recipes, new_recipes)
-          new_recipes
+          [_, recipe] = new_recipes = [1, new_recipe - 10]
+          {new_recipes, <<recipes::binary, 1, recipe>>}
       end
 
     elf1_pos = rem(elf1_pos + 1 + elf1_recipe, num_recipes(recipes))
     elf2_pos = rem(elf2_pos + 1 + elf2_recipe, num_recipes(recipes))
-    {previous_new_recipes, {elf1_pos, elf2_pos, recipes, new_recipes}}
+
+    state(elf1_pos: elf1_pos, elf2_pos: elf2_pos, recipes: recipes, new_recipes: new_recipes)
   end
 
-  defp add_recipes(recipes, new_recipes) do
-    new_recipes
-    |> Stream.with_index(num_recipes(recipes))
-    |> Enum.each(fn {recipe, index} -> :ets.insert(recipes, {index, recipe}) end)
-  end
+  defp num_recipes(recipes), do: byte_size(recipes)
 
-  defp num_recipes(recipes), do: :ets.info(recipes, :size)
-
-  defp recipe_at(recipes, position) do
-    [{^position, recipe}] = :ets.lookup(recipes, position)
-    recipe
-  end
+  defp recipe_at(recipes, position), do: :binary.at(recipes, position)
 end
