72978bd27db4db275badcc572aa0f80b269412d1
Sasa Juric <sasa.juric@gmail.com>
2018-12-20 15:48:18 +0100

    simplify 201820

--- lib/2018/day20.ex
+++ lib/2018/day20.ex
@@ -13,64 +13,41 @@ defmodule Aoc201820 do
 
   defp instructions(), do: Aoc.input_file(2018, 20) |> File.read!() |> Parser.ast()
 
-  defp walk(instructions, state \\ %{moves: 0, pos: {0, 0}, map: %{}})
+  defp walk(state \\ %{positions: %{{0, 0} => 0}, map: %{}}, instructions),
+    do: Enum.reduce(instructions, state, &apply_instruction/2)
 
-  defp walk([], state), do: state
+  defp apply_instruction({:move, directions}, state), do: Enum.reduce(directions, state, &move/2)
+  defp apply_instruction({:choice, type, choices}, state), do: explore_choices(type, choices, state)
 
-  defp walk([{:choice, :optional, choices} | instructions_after_choices], state) do
+  defp move(direction, state),
+    do: merge_positions(%{state | positions: %{}}, state.positions |> Stream.map(&offset(&1, direction)))
+
+  defp offset({{x, y}, distance}, :east), do: {{x + 1, y}, distance + 1}
+  defp offset({{x, y}, distance}, :west), do: {{x - 1, y}, distance + 1}
+  defp offset({{x, y}, distance}, :north), do: {{x, y + 1}, distance + 1}
+  defp offset({{x, y}, distance}, :south), do: {{x, y - 1}, distance + 1}
+
+  defp explore_choices(choices_type, choices, state_before_choice) do
     Enum.reduce(
       choices,
-      put_in(state.map, walk(instructions_after_choices, state).map),
-      fn choice_instructions, previous_state ->
-        new_state = walk(choice_instructions, previous_state)
-
-        # This is the key optimization which drastically reduces the number of combinations. If, after applying the
-        # instructions of this choice, we end up in a place we've already visited, and the new score is not better than
-        # the previous one, there's no need to try instructions after choices (we've already tried them with a better
-        # score).
-        map =
-          if Map.has_key?(previous_state.map, new_state.pos) and new_state.moves >= previous_state.moves,
-            do: new_state.map,
-            else: walk(instructions_after_choices, new_state).map
-
-        put_in(previous_state.map, map)
+      if(choices_type == :optional, do: state_before_choice, else: %{state_before_choice | positions: %{}}),
+      fn choice, state ->
+        %{state | positions: state_before_choice.positions}
+        |> walk(choice)
+        |> merge_positions(state.positions)
       end
     )
   end
 
-  defp walk([{:choice, :mandatory, choices} | instruction_after_choices], state) do
-    # We could apply a similar optimization to the one in the :optional branch. However, the optimization would have to
-    # be a bit different: for the best score of every end position we'd need to walk through remaining choices.
-    # Since the :optional optimization reduced the running time considerably, this branch is left unoptimized.
-    Enum.reduce(
-      choices,
-      state,
-      fn choice_instructions, original_state ->
-        new_state = walk(choice_instructions, original_state)
-        map = walk(instruction_after_choices, new_state).map
-        put_in(original_state.map, map)
-      end
-    )
+  defp merge_positions(state, new_positions) do
+    new_positions = new_positions |> Stream.map(&with_shortest_distance(&1, state)) |> Enum.into(state.positions)
+    %{state | positions: new_positions, map: Map.merge(state.map, new_positions)}
   end
 
-  defp walk([{:move, directions} | remaining_instructions], state) do
-    state = apply_moves(directions, state)
-    walk(remaining_instructions, state)
+  defp with_shortest_distance({pos, distance}, state) do
+    distance = Enum.min([distance, Map.get(state.map, pos, distance), Map.get(state.positions, pos, distance)])
+    {pos, distance}
   end
-
-  defp apply_moves(moves, state), do: Enum.reduce(moves, state, &apply_move(&2, &1))
-
-  defp apply_move(state, direction) do
-    moves = state.moves + 1
-    pos = move(state.pos, direction) |> move(direction)
-    map = Map.update(state.map, pos, moves, &min(&1, moves))
-    %{state | moves: moves, pos: pos, map: map}
-  end
-
-  defp move({x, y}, :east), do: {x + 1, y}
-  defp move({x, y}, :west), do: {x - 1, y}
-  defp move({x, y}, :north), do: {x, y + 1}
-  defp move({x, y}, :south), do: {x, y - 1}
 
   defmodule Parser do
     def ast(string), do: string |> tokens() |> build_ast()
@@ -90,8 +67,8 @@ defmodule Aoc201820 do
     end
 
     defp route_element([{:move, _} | _] = tokens) do
-      {moves, tokens} = Enum.split_while(tokens, &match?({:move, _}, &1))
-      directions = Enum.map(moves, fn {:move, direction} -> direction end)
+      {distance, tokens} = Enum.split_while(tokens, &match?({:move, _}, &1))
+      directions = Enum.map(distance, fn {:move, direction} -> direction end)
       {{:move, directions}, tokens}
     end
 
