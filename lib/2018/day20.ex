e0fd3e5489a03466e5bf2c26524ac9d64e06a74f
Sasa Juric <sasa.juric@gmail.com>
2018-12-20 23:14:09 +0100

    simplify 201820

--- lib/2018/day20.ex
+++ lib/2018/day20.ex
@@ -2,8 +2,8 @@ defmodule Aoc201820 do
   alias Aoc201820.Parser
 
   def run() do
-    IO.inspect(part1())
-    IO.inspect(part2())
+    IO.puts(part1())
+    IO.puts(part2())
   end
 
   defp part1(), do: final_map() |> Map.values() |> Enum.max()
@@ -13,41 +13,45 @@ defmodule Aoc201820 do
 
   defp instructions(), do: Aoc.input_file(2018, 20) |> File.read!() |> Parser.ast()
 
-  defp walk(state \\ %{positions: %{{0, 0} => 0}, map: %{}}, instructions),
+  defp walk(state \\ %{positions: MapSet.new([{0, 0}]), map: %{{0, 0} => 0}}, instructions),
     do: Enum.reduce(instructions, state, &apply_instruction/2)
 
   defp apply_instruction({:move, directions}, state), do: Enum.reduce(directions, state, &move/2)
   defp apply_instruction({:choice, type, choices}, state), do: explore_choices(type, choices, state)
 
-  defp move(direction, state),
-    do: merge_positions(%{state | positions: %{}}, state.positions |> Stream.map(&offset(&1, direction)))
+  defp move(direction, state) do
+    new_positions =
+      state.positions
+      |> Stream.map(&{&1, Map.fetch!(state.map, &1)})
+      |> Enum.map(fn {pos, distance} -> {offset(pos, direction), distance + 1} end)
 
-  defp offset({{x, y}, distance}, :east), do: {{x + 1, y}, distance + 1}
-  defp offset({{x, y}, distance}, :west), do: {{x - 1, y}, distance + 1}
-  defp offset({{x, y}, distance}, :north), do: {{x, y + 1}, distance + 1}
-  defp offset({{x, y}, distance}, :south), do: {{x, y - 1}, distance + 1}
+    # Only putting the new positions into the map. If a position already exists, the stored distance is surely smaller
+    # than the one we have here.
+    map = Enum.reduce(new_positions, state.map, fn {pos, distance}, map -> Map.put_new(map, pos, distance) end)
+    new_positions = new_positions |> Stream.map(fn {pos, _distance} -> pos end) |> MapSet.new()
+
+    %{state | map: map, positions: new_positions}
+  end
+
+  defp offset({x, y}, :east), do: {x + 1, y}
+  defp offset({x, y}, :west), do: {x - 1, y}
+  defp offset({x, y}, :north), do: {x, y + 1}
+  defp offset({x, y}, :south), do: {x, y - 1}
 
   defp explore_choices(choices_type, choices, state_before_choice) do
     Enum.reduce(
       choices,
-      if(choices_type == :optional, do: state_before_choice, else: %{state_before_choice | positions: %{}}),
+      if(choices_type == :optional, do: state_before_choice, else: %{state_before_choice | positions: MapSet.new()}),
       fn choice, state ->
         %{state | positions: state_before_choice.positions}
         |> walk(choice)
-        |> merge_positions(state.positions)
+        |> merge_positions(state)
       end
     )
   end
 
-  defp merge_positions(state, new_positions) do
-    new_positions = new_positions |> Stream.map(&with_shortest_distance(&1, state)) |> Enum.into(state.positions)
-    %{state | positions: new_positions, map: Map.merge(state.map, new_positions)}
-  end
-
-  defp with_shortest_distance({pos, distance}, state) do
-    distance = Enum.min([distance, Map.get(state.map, pos, distance), Map.get(state.positions, pos, distance)])
-    {pos, distance}
-  end
+  defp merge_positions(target_state, source_state),
+    do: update_in(target_state.positions, &MapSet.union(&1, source_state.positions))
 
   defmodule Parser do
     def ast(string), do: string |> tokens() |> build_ast()
