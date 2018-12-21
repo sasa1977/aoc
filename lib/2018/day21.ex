1e8abe1f92bdd775d1ded47f5fd7552f8e826231
Sasa Juric <sasa.juric@gmail.com>
2018-12-21 12:24:21 +0100

    extract device into its own module

--- lib/2018/day21.ex
+++ lib/2018/day21.ex
@@ -1,4 +1,6 @@
 defmodule Aoc201821 do
+  alias Aoc2018.Device
+
   def run() do
     IO.puts(part1())
     IO.puts(part2())
@@ -8,89 +10,30 @@ defmodule Aoc201821 do
   defp part2(), do: Aoc.EnumHelper.last(terminating_inputs())
 
   defp terminating_inputs() do
-    all_machine_states()
+    Aoc.input_file(2018, 21)
+    |> Device.from_file()
+    |> Device.all_states(optimize: %{18 => &optimized_inner_loop/1})
     # according to the input program, termination happens in instruction 28, if input (r0) == r1
     |> Stream.filter(&(&1.ip == 28))
-    |> Stream.transform(MapSet.new(), fn machine, seen_states ->
-      # if we're on the same instruction with the same registers, the machine will cycle, so we stop here
-      if MapSet.member?(seen_states, machine.registers),
+    |> Stream.transform(MapSet.new(), fn device, seen_states ->
+      # if we're on the same instruction with the same registers, the device will cycle, so we stop here
+      if MapSet.member?(seen_states, device.registers),
         do: {:halt, seen_states},
-        else: {[machine], MapSet.put(seen_states, machine.registers)}
+        else: {[device], MapSet.put(seen_states, device.registers)}
     end)
-    |> Stream.map(&get_reg(&1, 1))
+    |> Stream.map(&Device.register(&1, 1))
     |> Stream.uniq()
   end
 
-  defp all_machine_states(), do: program() |> new_machine() |> Stream.iterate(&exec_next_instruction/1)
-
-  defp program() do
-    Aoc.input_lines(2018, 21)
-    |> Stream.map(&String.replace(&1, ~r/;.*$/, ""))
-    |> Stream.map(&String.trim/1)
-    |> Stream.reject(&(&1 == ""))
-    |> Enum.map(&parse_instruction/1)
-  end
-
-  defp parse_instruction(<<"#ip ", register>>), do: {:bind_ip, register - ?0}
-
-  defp parse_instruction(<<op::binary-size(4), " ", args::binary>>) do
-    args = String.replace(args, ~r/#.*/, "")
-    {String.to_existing_atom(op), args |> String.split() |> Enum.map(&String.to_integer/1)}
-  end
-
-  defp new_machine([{:bind_ip, register} | instructions]) do
-    %{
-      ip: 0,
-      ip_register: register,
-      instructions: List.to_tuple(instructions),
-      registers: 0..5 |> Stream.map(&{&1, 0}) |> Map.new()
-    }
-  end
-
-  defp exec_next_instruction(%{ip: 18} = machine) do
-    d = get_reg(machine, 3)
-    f = get_reg(machine, 5)
+  defp optimized_inner_loop(%{ip: 18} = device) do
+    # optimized version of the inner loop, see comments in the input file for details
+    d = Device.register(device, 3)
+    f = Device.register(device, 5)
     d = if d > f, do: d, else: div(f, 256)
 
-    machine
-    |> put_reg(2, 1)
-    |> put_reg(3, d)
-    |> put_reg(machine.ip_register, 25)
-    |> update_ip()
-  end
-
-  defp exec_next_instruction(machine) do
-    machine
-    |> put_reg(machine.ip_register, machine.ip)
-    |> exec(elem(machine.instructions, machine.ip))
-    |> update_ip()
-  end
-
-  defp update_ip(machine), do: %{machine | ip: get_reg(machine, machine.ip_register) + 1}
-
-  defp get_reg(machine, pos), do: Map.fetch!(machine.registers, pos)
-  defp put_reg(machine, pos, value), do: %{machine | registers: Map.put(machine.registers, pos, value)}
-
-  defp exec(machine, {instruction, [a, b, c]}), do: Map.fetch!(instructions(), instruction).(machine, a, b, c)
-
-  defp instructions() do
-    %{
-      addr: &put_reg(&1, &4, get_reg(&1, &2) + get_reg(&1, &3)),
-      addi: &put_reg(&1, &4, get_reg(&1, &2) + &3),
-      mulr: &put_reg(&1, &4, get_reg(&1, &2) * get_reg(&1, &3)),
-      muli: &put_reg(&1, &4, get_reg(&1, &2) * &3),
-      banr: &put_reg(&1, &4, :erlang.band(get_reg(&1, &2), get_reg(&1, &3))),
-      bani: &put_reg(&1, &4, :erlang.band(get_reg(&1, &2), &3)),
-      borr: &put_reg(&1, &4, :erlang.bor(get_reg(&1, &2), get_reg(&1, &3))),
-      bori: &put_reg(&1, &4, :erlang.bor(get_reg(&1, &2), &3)),
-      gtir: &put_reg(&1, &4, if(&2 > get_reg(&1, &3), do: 1, else: 0)),
-      gtri: &put_reg(&1, &4, if(get_reg(&1, &2) > &3, do: 1, else: 0)),
-      gtrr: &put_reg(&1, &4, if(get_reg(&1, &2) > get_reg(&1, &3), do: 1, else: 0)),
-      eqir: &put_reg(&1, &4, if(&2 == get_reg(&1, &3), do: 1, else: 0)),
-      eqri: &put_reg(&1, &4, if(get_reg(&1, &2) == &3, do: 1, else: 0)),
-      eqrr: &put_reg(&1, &4, if(get_reg(&1, &2) == get_reg(&1, &3), do: 1, else: 0)),
-      setr: fn machine, reg_a, _b, reg_c -> put_reg(machine, reg_c, get_reg(machine, reg_a)) end,
-      seti: fn machine, val, _b, reg_c -> put_reg(machine, reg_c, val) end
-    }
+    device
+    |> Device.put_register(2, 1)
+    |> Device.put_register(3, d)
+    |> Device.set_next_instruction(26)
   end
 end
---
+++ lib/2018/device.ex
@@ -0,0 +1,73 @@
+defmodule Aoc2018.Device do
+  def from_file(file) do
+    file
+    |> File.stream!()
+    |> Stream.map(&String.replace(&1, ~r/;.*$/, ""))
+    |> Stream.map(&String.trim/1)
+    |> Stream.reject(&(&1 == ""))
+    |> Enum.map(&parse_instruction/1)
+    |> new()
+  end
+
+  def all_states(device, opts \\ []), do: Stream.iterate(device, &next_state(&1, opts))
+
+  def register(device, pos), do: Map.fetch!(device.registers, pos)
+  def put_register(device, pos, value), do: %{device | registers: Map.put(device.registers, pos, value)}
+
+  def set_next_instruction(device, pos), do: next_instruction(put_register(device, device.ip_register, pos - 1))
+
+  defp parse_instruction(<<"#ip ", register>>), do: {:bind_ip, register - ?0}
+
+  defp parse_instruction(<<op::binary-size(4), " ", args::binary>>) do
+    args = String.replace(args, ~r/#.*/, "")
+    {String.to_existing_atom(op), args |> String.split() |> Enum.map(&String.to_integer/1)}
+  end
+
+  defp new([{:bind_ip, register} | instructions]) do
+    %{
+      ip: 0,
+      ip_register: register,
+      instructions: List.to_tuple(instructions),
+      registers: 0..5 |> Stream.map(&{&1, 0}) |> Map.new()
+    }
+  end
+
+  defp next_state(device, opts) do
+    case Map.fetch(Keyword.get(opts, :optimize, %{}), device.ip) do
+      :error -> exec_next_instruction(device)
+      {:ok, optimized} -> optimized.(device)
+    end
+  end
+
+  defp exec_next_instruction(device) do
+    device
+    |> put_register(device.ip_register, device.ip)
+    |> exec(elem(device.instructions, device.ip))
+    |> next_instruction()
+  end
+
+  defp next_instruction(device), do: %{device | ip: register(device, device.ip_register) + 1}
+
+  defp exec(device, {instruction, [a, b, c]}), do: Map.fetch!(instructions(), instruction).(device, a, b, c)
+
+  defp instructions() do
+    %{
+      addr: &put_register(&1, &4, register(&1, &2) + register(&1, &3)),
+      addi: &put_register(&1, &4, register(&1, &2) + &3),
+      mulr: &put_register(&1, &4, register(&1, &2) * register(&1, &3)),
+      muli: &put_register(&1, &4, register(&1, &2) * &3),
+      banr: &put_register(&1, &4, :erlang.band(register(&1, &2), register(&1, &3))),
+      bani: &put_register(&1, &4, :erlang.band(register(&1, &2), &3)),
+      borr: &put_register(&1, &4, :erlang.bor(register(&1, &2), register(&1, &3))),
+      bori: &put_register(&1, &4, :erlang.bor(register(&1, &2), &3)),
+      gtir: &put_register(&1, &4, if(&2 > register(&1, &3), do: 1, else: 0)),
+      gtri: &put_register(&1, &4, if(register(&1, &2) > &3, do: 1, else: 0)),
+      gtrr: &put_register(&1, &4, if(register(&1, &2) > register(&1, &3), do: 1, else: 0)),
+      eqir: &put_register(&1, &4, if(&2 == register(&1, &3), do: 1, else: 0)),
+      eqri: &put_register(&1, &4, if(register(&1, &2) == &3, do: 1, else: 0)),
+      eqrr: &put_register(&1, &4, if(register(&1, &2) == register(&1, &3), do: 1, else: 0)),
+      setr: fn device, reg_a, _b, reg_c -> put_register(device, reg_c, register(device, reg_a)) end,
+      seti: fn device, val, _b, reg_c -> put_register(device, reg_c, val) end
+    }
+  end
+end
