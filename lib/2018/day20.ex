37abecc9ce4a9327b382372d08e9756f7c8ddd70
Sasa Juric <sasa.juric@gmail.com>
2018-12-26 22:20:06 +0100

    improve 201820
    The original solution was wrong. It just happened to produce the correct result simple inputs.

--- lib/2018/day20.ex
+++ lib/2018/day20.ex
@@ -6,112 +6,139 @@ defmodule Aoc201820 do
     IO.puts(part2())
   end
 
-  defp part1(), do: final_map() |> Map.values() |> Enum.max()
-  defp part2(), do: final_map() |> Map.values() |> Stream.filter(&(&1 >= 1000)) |> Enum.count()
+  defp part1(), do: rooms() |> Stream.map(& &1.distance) |> Enum.max()
+  defp part2(), do: rooms() |> Stream.filter(&(&1.distance >= 1000)) |> Enum.count()
 
-  defp final_map(), do: walk(instructions()).map
-
-  defp instructions(), do: Aoc.input_file(2018, 20) |> File.read!() |> Parser.ast()
-
-  defp walk(state \\ %{positions: MapSet.new([{0, 0}]), map: %{{0, 0} => 0}}, instructions),
-    do: Enum.reduce(instructions, state, &apply_instruction/2)
-
-  defp apply_instruction({:move, directions}, state), do: Enum.reduce(directions, state, &move/2)
-  defp apply_instruction({:choice, type, choices}, state), do: explore_choices(type, choices, state)
-
-  defp move(direction, state) do
-    new_positions =
-      state.positions
-      |> Stream.map(&{&1, Map.fetch!(state.map, &1)})
-      |> Enum.map(fn {pos, distance} -> {offset(pos, direction), distance + 1} end)
-
-    # Only putting the new positions into the map. If a position already exists, the stored distance is surely smaller
-    # than the one we have here.
-    map = Enum.reduce(new_positions, state.map, fn {pos, distance}, map -> Map.put_new(map, pos, distance) end)
-    new_positions = new_positions |> Stream.map(fn {pos, _distance} -> pos end) |> MapSet.new()
-
-    %{state | map: map, positions: new_positions}
+  def rooms() do
+    Aoc.input_file(2018, 20)
+    |> File.read!()
+    |> String.trim()
+    |> Parser.instructions()
+    |> collect_neighbours()
+    |> collect_rooms()
   end
 
-  defp offset({x, y}, :east), do: {x + 1, y}
-  defp offset({x, y}, :west), do: {x - 1, y}
-  defp offset({x, y}, :north), do: {x, y + 1}
-  defp offset({x, y}, :south), do: {x, y - 1}
+  defp collect_rooms(neighbours) do
+    %{distance: 0, next_rooms: MapSet.new([{0, 0}]), neighbours: neighbours}
+    |> Stream.unfold(&{current_rooms(&1), next_rooms(&1)})
+    |> Stream.take_while(&(not Enum.empty?(&1)))
+    |> Stream.concat()
+  end
 
-  defp explore_choices(choices_type, choices, state_before_choice) do
+  defp current_rooms(collect_rooms_state),
+    do: Enum.map(collect_rooms_state.next_rooms, &%{distance: collect_rooms_state.distance, at: &1})
+
+  defp next_rooms(collect_rooms_state) do
     Enum.reduce(
-      choices,
-      if(choices_type == :optional, do: state_before_choice, else: %{state_before_choice | positions: MapSet.new()}),
-      fn choice, state ->
-        %{state | positions: state_before_choice.positions}
-        |> walk(choice)
-        |> merge_positions(state)
+      collect_rooms_state.next_rooms,
+      %{collect_rooms_state | next_rooms: MapSet.new(), distance: collect_rooms_state.distance + 1},
+      &move_to_neighbours/2
+    )
+  end
+
+  defp move_to_neighbours(room, collect_rooms_state) do
+    next_rooms =
+      collect_rooms_state.neighbours
+      |> Map.fetch!(room)
+      |> Enum.filter(&Map.has_key?(collect_rooms_state.neighbours, &1))
+      |> MapSet.new()
+      |> MapSet.union(collect_rooms_state.next_rooms)
+
+    neighbours = Map.delete(collect_rooms_state.neighbours, room)
+
+    %{collect_rooms_state | neighbours: neighbours, next_rooms: next_rooms}
+  end
+
+  defp collect_neighbours(instructions),
+    do: collect_neighbours(instructions, %{positions: MapSet.new([{0, 0}]), neighbours: %{}}).neighbours
+
+  defp collect_neighbours(instructions, collect_neighbours_state),
+    do: Enum.reduce(instructions, collect_neighbours_state, &process_instruction/2)
+
+  defp process_instruction({:move, direction}, collect_neighbours_state) do
+    Enum.reduce(
+      collect_neighbours_state.positions,
+      %{collect_neighbours_state | positions: MapSet.new()},
+      &record_move(&2, &1, move(&1, direction))
+    )
+  end
+
+  defp process_instruction({:switch, type, branches}, state_before_switch) do
+    Enum.reduce(
+      branches,
+      if(type == :mandatory, do: %{state_before_switch | positions: MapSet.new()}, else: state_before_switch),
+      fn branch_instructions, state_before_branch ->
+        branch_instructions
+        |> collect_neighbours(%{state_before_branch | positions: state_before_switch.positions})
+        |> update_in([:positions], &MapSet.union(&1, state_before_branch.positions))
       end
     )
   end
 
-  defp merge_positions(target_state, source_state),
-    do: update_in(target_state.positions, &MapSet.union(&1, source_state.positions))
+  defp record_move(collect_neighbours_state, pos1, pos2) do
+    positions = MapSet.put(collect_neighbours_state.positions, pos2)
+
+    neighbours =
+      collect_neighbours_state.neighbours
+      |> Map.update(pos1, MapSet.new([pos2]), &MapSet.put(&1, pos2))
+      |> Map.update(pos2, MapSet.new([pos1]), &MapSet.put(&1, pos1))
+
+    %{collect_neighbours_state | positions: positions, neighbours: neighbours}
+  end
+
+  defp move({x, y}, :north), do: {x, y + 1}
+  defp move({x, y}, :south), do: {x, y - 1}
+  defp move({x, y}, :east), do: {x + 1, y}
+  defp move({x, y}, :west), do: {x - 1, y}
 
   defmodule Parser do
-    def ast(string), do: string |> tokens() |> build_ast()
-
-    defp build_ast([:route_start | tokens]) do
-      {ast, [:route_end]} = route(tokens)
-      ast
+    def instructions("^" <> rest) do
+      {instructions, "$"} = parse_instructions(rest)
+      instructions
     end
 
-    defp route(tokens) do
-      with {element, tokens} <- route_element(tokens) do
-        case route(tokens) do
-          nil -> {[element], tokens}
-          {other_elements, tokens} -> {[element | other_elements], tokens}
+    defp parse_instructions(input) do
+      case next_element(input) do
+        nil ->
+          {[], input}
+
+        {element, rest} ->
+          {remaining_elements, rest} = parse_instructions(rest)
+          {[element | remaining_elements], rest}
+      end
+    end
+
+    defp next_element("N" <> rest), do: {{:move, :north}, rest}
+    defp next_element("S" <> rest), do: {{:move, :south}, rest}
+    defp next_element("E" <> rest), do: {{:move, :east}, rest}
+    defp next_element("W" <> rest), do: {{:move, :west}, rest}
+    defp next_element("(" <> rest), do: parse_switch(rest)
+    defp next_element(_unknown), do: nil
+
+    defp parse_switch(input) do
+      {branches, rest} = parse_branches(input)
+
+      {switch_type, rest} =
+        case rest do
+          ")" <> rest -> {:mandatory, rest}
+          "|)" <> rest -> {:optional, rest}
         end
+
+      {{:switch, switch_type, branches}, rest}
+    end
+
+    defp parse_branches(input) do
+      case parse_instructions(input) do
+        {branch, ")" <> _ = rest} ->
+          {[branch], rest}
+
+        {branch, "|)" <> _ = rest} ->
+          {[branch], rest}
+
+        {branch, "|" <> rest} ->
+          {remaining_branches, rest} = parse_branches(rest)
+          {[branch | remaining_branches], rest}
       end
     end
-
-    defp route_element([{:move, _} | _] = tokens) do
-      {distance, tokens} = Enum.split_while(tokens, &match?({:move, _}, &1))
-      directions = Enum.map(distance, fn {:move, direction} -> direction end)
-      {{:move, directions}, tokens}
-    end
-
-    defp route_element([:choice_start | tokens]) do
-      {routes, tokens} = routes(tokens)
-
-      case tokens do
-        [:or, :choice_end | tokens] -> {{:choice, :optional, routes}, tokens}
-        [:choice_end | tokens] -> {{:choice, :mandatory, routes}, tokens}
-      end
-    end
-
-    defp route_element(_tokens), do: nil
-
-    defp routes(tokens) do
-      with {route, tokens} <- route(tokens) do
-        case tokens do
-          [:or | tokens] = outer_tokens ->
-            case routes(tokens) do
-              nil -> {[route], outer_tokens}
-              {other_routes, tokens} -> {[route | other_routes], tokens}
-            end
-
-          _ ->
-            {[route], tokens}
-        end
-      end
-    end
-
-    defp tokens(string), do: string |> String.trim() |> to_charlist() |> Enum.map(&token/1)
-
-    defp token(?^), do: :route_start
-    defp token(?$), do: :route_end
-    defp token(?(), do: :choice_start
-    defp token(?)), do: :choice_end
-    defp token(?|), do: :or
-    defp token(?N), do: {:move, :north}
-    defp token(?S), do: {:move, :south}
-    defp token(?E), do: {:move, :east}
-    defp token(?W), do: {:move, :west}
   end
 end
